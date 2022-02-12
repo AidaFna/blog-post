e8512a59490488776f8dfd7f44c3ca175a6d164f
AidaFna <93112554+AidaFna@users.noreply.github.com>
2022-02-12 14:45:37 +0700

    fix bugs

--- components/modal/update.jsx
+++ components/modal/update.jsx
@@ -28,7 +28,7 @@ const ModalUpdate = (props) => {
     // title errors
     if (!title || title.trim() === "") newErrors.title = "cannot be blank!";
     else if (title.length > 50)
-      newErrors.name = "title cannot be more than 50 characters!";
+      newErrors.title = "title cannot be more than 50 characters!";
     // content errors
     if (!content || content.trim() === "")
       newErrors.content = "cannot be blank!";
